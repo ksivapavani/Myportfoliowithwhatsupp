2a22a7c911cc27e3ed1eb14e5f16f54966275e12
ksivapavani <127827351+ksivapavani@users.noreply.github.com>
2023-05-20 10:53:06 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+    $('.about-info-inner').typed
+    ({
+      strings: [
+      "<span> Hi, I am Kottapalli Siva Pavani i am post graduate student and certified by the jspiders in JAVA FULL STACK DEVELOPNMENT",
+           "<span> i WANT to become FULL STACK DEVELOPER and to become a CEO ",
+           "<span> i believe that a person who write or create a code is easy compares to a person who REWRITE OR DEBUG THE CODE in any INDUSTRY",
+           "<span>  EXACTLY LIKE WHO AS ITS OWN IDEA ITS EASY TO TELL.BUT WHILE PUTTING INTO REALITY ITS DIFFICULT"],
+           typeSpeed : '1',
+      contentType : 'html',
+    });
+  }, 100);
